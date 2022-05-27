fa38d84cc92df709a8d7b54cbfa6da8bf6de06cc
Vishal kumar <vishalk80269@gmail.com>
2022-05-27 14:28:22 +0530

    creat

---
+++ Jsx/hire.js
@@ -0,0 +1,55 @@
+const hires=[
+    {
+        id:"1",
+       src:"./Images/3.jpg",
+        title1:"Omolola Oluwatosin Ibojo",
+      title2:"Omolola was a midwife and wanted to build tech skills to change career. Apriori gave her the opportunity to do that. She is now a full-stack Developer at Patchwork Health.",
+        links:"Watch Omolola Oluwatosin's story",
+    },
+    {
+        id:"2",
+        src:"./Images/4.jpg",
+        title1:"Quitterie Mathelin-Moreauxs",
+        title2:"A Business School graduate who wanted to be an entrepreneur, Quitterie had an idea, but needed the tech skills and the right partner to build it with. At Apriori, she found both.",
+        links:"Watch Quitterie's story",
+    },
+    {
+        id:"3",
+        src:"./Images/5.jpg",
+        title1:"Emanuel Coen",
+        title2:"Emanuel started off his career at PayPal, became interested in cryptocurrencies and recently launched a side project aiming at educating people on the topic.",
+        links:"Read Emanuel's story",
+    },
+
+]
+
+const dataB=document.getElementById("two");
+hires.map((item,index)=>(
+    dataB.innerHTML += `
+    <div class="col-md-4 hoverEffect ">
+    <div class="row ">
+        <img src=${item.src} alt="story-1" class="img-fluid  bradius">
+    </div>
+    <div class="row text-start mt-2">
+        <h5
+        style="font-weight: 900;"
+        >
+           ${item.title1}
+        </h5>
+    </div>
+    <div class="row text-start mt-2">
+        <p
+        style="font-size: larger;opacity: 0.9;"
+        >
+        ${item.title2}
+        </p>
+    </div>
+    <div class="row text-start mt-2 link3">
+        <a href="#">
+          ${item.links}
+        </a>
+    
+    </div>
+</div>
+      `
+))
---
+++ Jsx/life.js
@@ -0,0 +1,59 @@
+const lifes=[
+    {
+        id:"1",
+        number:"4.99/5",
+        title1:"average review",
+        title2:"We are focused on our students' experience: our bootcamps have the best student reviews and Net Promoter Score (NPS) of all coding bootcamps.",
+        links:"What our student say",
+    },
+    {
+        id:"2",
+        number:"14,863",
+        title1:"graduates",
+        title2:"Apriori is a strong community of developers, designers, entrepreneurs and tech talent, creating unique opportunities for our alumni worldwide.",
+        links:"Meet our garduate",
+    },
+    {
+        id:"3",
+        number:"93%",
+        title1:"employment rate",
+        title2:"This is the percentage of our H2 2020 alumni who have found employment according to our jobs report. Other data include job roles, salaries, and time spent searching.",
+        links:"Discover our Jobs Report",
+    },
+    {
+        id:"4",
+        number:"190+",
+        title1:"startups created",
+        title2:"We teach our students how to build great apps. Our alumni have launched successful startups, some of them raising up to $500M or even being acquiblue by Apple.",
+        links:"Discover alumni startups",
+    },
+]
+
+const data=document.getElementById("one");
+lifes.map((item,index)=>(
+    data.innerHTML += `
+    <div class="col-md-5 mt-4">
+                <div class="row">
+                 <h1
+                 style="font-weight: bold;color:#0152a8 ;"
+                 >${item.number}</h1>
+                </div>
+                <div class="row">
+            <p
+            style="font-size:larger ;"
+            ><b> ${item.title1}</b></p>
+                </div>
+                <div class="row">
+                    <p
+                    style="font-size:large ;opacity: 0.9;"
+                    >
+                    ${item.title2}
+
+                          </p>
+                </div>
+                <div class="row text-start a2de">
+                  <a href="#">${item.links}</a>
+                </div>
+            </div>
+      `
+))
