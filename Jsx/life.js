336d6f5a8a51313bacef526d286fdd2ca0d91093
Vishal kumar <vishalk80269@gmail.com>
2022-05-31 15:53:04 +0530

    typing text add

--- Jsx/life.js
+++ Jsx/life.js
@@ -1,7 +1,8 @@
 const lifes=[
     {
         id:"1",
-        number:"4.99/5",
+        number:"4.99",
+        symbol:"/5",
         title1:"average review",
         title2:"We are focused on our students' experience: our bootcamps have the best student reviews and Net Promoter Score (NPS) of all coding bootcamps.",
         links:"What our student say",
@@ -9,20 +10,23 @@ const lifes=[
     {
         id:"2",
         number:"14,863",
+        symbol:" ",
         title1:"graduates",
         title2:"Apriori is a strong community of developers, designers, entrepreneurs and tech talent, creating unique opportunities for our alumni worldwide.",
         links:"Meet our garduates",
     },
     {
         id:"3",
-        number:"93%",
+        number:"93",
+        symbol:"%",
         title1:"employment rate",
         title2:"This is the percentage of our H2 2020 alumni who have found employment according to our jobs report. Other data include job roles, salaries, and time spent searching.",
         links:"Discover our Jobs Report",
     },
     {
         id:"4",
-        number:"190+",
+        number:"190",
+        symbol:"+",
         title1:"startups created",
         title2:"We teach our students how to build great apps. Our alumni have launched successful startups, some of them raising up to $500M or even being acquiblue by Apple.",
         links:"Discover alumni startups",
@@ -33,10 +37,16 @@ const data=document.getElementById("one");
 lifes.map((item,index)=>(
     data.innerHTML += `
     <div class="col-md-5 mt-4">
-                <div class="row at767cen">
-                 <h1
-                 style="font-weight:bolder;color:#0152a8 ; font-size:xx-larger;"
-                 >${item.number}</h1>
+                <div class="row at767cen ">
+                <div class="col  ">
+                <h1 class="pet c"
+                style="font-weight:bolder;color:#0152a8 ; font-size:xx-larger;"
+                >${item.number}</h1>
+                <h1 class="sym c" style="font-weight:bolder;color:#0152a8 ; font-size:xx-larger;"
+                >${item.symbol}</h1>
+                </div>
+                
+               
                 </div>
                 <div class="row at767cen">
             <p
@@ -57,3 +67,4 @@ lifes.map((item,index)=>(
             </div>
       `
 ))
+
