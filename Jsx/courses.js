550bf20d5c9f382f3006fbecb868e8fc45cf10fa
Vishal kumar <vishalk80269@gmail.com>
2022-06-02 04:57:27 +0530

    create courses pages

---
+++ Jsx/courses.js
@@ -0,0 +1,224 @@
+// jquery functions use for hide and show
+
+$( ".test" ).click(function() {
+    $( ".used" ).toggle( "slow" );
+  });
+
+  $( ".test1" ).click(function() {
+    $( ".used1" ).toggle( "slow" );
+  });
+
+  $( ".test2" ).click(function() {
+    $( ".used2" ).toggle( "slow" );
+  });
+
+  $( ".test3" ).click(function() {
+    $( ".used3" ).toggle( "slow" );
+  });
+
+  $( ".test4" ).click(function() {
+    $( ".used4" ).toggle( "slow" );
+  });
+
+const courses=[
+    {
+        id:"1",
+        src:"./Images/python.jpg",
+        btns:"PYTHON",
+        class1:"doClick",
+        title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatisplaceat exercitationem veniam quae iure facere voluptatum nam doloribus. Beatae nemo,ullam unde necessitatibus vel ex assumenda quae impedit fugiat optio.   Obcaecati, illum, esse illo similique qu numquam iure omnis velit sed id  dignissimos officiis corporis exercitationem suscipit, libero doloremque amet modi a voluptatum sit! Voluptatibus pariatur facilis quidem aliquam asperiores?",
+    },
+    {
+        id:"2",
+        src:"./Images/python.jpg",
+        btns:"WEB DEVLOPMENT",
+        class1:"doClick1",
+        title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatisplaceat exercitationem veniam quae iure facere voluptatum nam doloribus. Beatae nemo,ullam unde necessitatibus vel ex assumenda quae impedit fugiat optio.   Obcaecati, illum, esse illo similique qu numquam iure omnis velit sed id  dignissimos officiis corporis exercitationem suscipit, libero doloremque amet modi a voluptatum sit! Voluptatibus pariatur facilis quidem aliquam asperiores?",
+    },
+    {
+        id:"3",
+        src:"./Images/python.jpg",
+        btns:"SPANISH",
+        class1:"doClick2",
+        title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatisplaceat exercitationem veniam quae iure facere voluptatum nam doloribus. Beatae nemo,ullam unde necessitatibus vel ex assumenda quae impedit fugiat optio.   Obcaecati, illum, esse illo similique qu numquam iure omnis velit sed id  dignissimos officiis corporis exercitationem suscipit, libero doloremque amet modi a voluptatum sit! Voluptatibus pariatur facilis quidem aliquam asperiores?",
+    },
+    {
+        id:"4",
+        src:"./Images/python.jpg",
+        btns:"GERMAN",
+        class1:"doClick3",
+        title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatisplaceat exercitationem veniam quae iure facere voluptatum nam doloribus. Beatae nemo,ullam unde necessitatibus vel ex assumenda quae impedit fugiat optio.   Obcaecati, illum, esse illo similique qu numquam iure omnis velit sed id  dignissimos officiis corporis exercitationem suscipit, libero doloremque amet modi a voluptatum sit! Voluptatibus pariatur facilis quidem aliquam asperiores?",
+    },
+    {
+        id:"5",
+        src:"./Images/python.jpg",
+        btns:"FRENCH",
+        class1:"doClick4",
+        title:"Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veritatisplaceat exercitationem veniam quae iure facere voluptatum nam doloribus. Beatae nemo,ullam unde necessitatibus vel ex assumenda quae impedit fugiat optio.   Obcaecati, illum, esse illo similique qu numquam iure omnis velit sed id  dignissimos officiis corporis exercitationem suscipit, libero doloremque amet modi a voluptatum sit! Voluptatibus pariatur facilis quidem aliquam asperiores?",
+    },
+   
+
+]
+
+
+document.getElementById('era').innerHTML=`${courses[0].btns}`;
+document.getElementById('er').innerHTML=`${courses[0].title}`;
+
+document.getElementById('era1').innerHTML=`${courses[1].btns}`;
+document.getElementById('er1').innerHTML=`${courses[1].title}`;
+
+
+document.getElementById('era2').innerHTML=`${courses[2].btns}`;
+document.getElementById('er2').innerHTML=`${courses[2].title}`;
+
+document.getElementById('era3').innerHTML=`${courses[3].btns}`;
+document.getElementById('er3').innerHTML=`${courses[3].title}`;
+
+document.getElementById('era4').innerHTML=`${courses[4].btns}`;
+document.getElementById('er4').innerHTML=`${courses[4].title}`;
+
+
+function checkP(){
+    document.getElementById('tired').style.display=="block"
+}
+function checkP1(){
+    document.getElementById('tired1').style.display=="block"
+}
+function checkP2(){
+    document.getElementById('tired2').style.display=="block"
+}
+function checkP3(){
+    document.getElementById('tired3').style.display=="block"
+}
+function checkP4(){
+    document.getElementById('tired4').style.display=="block"
+}
+
+
+// const dataY=document.getElementById("sold");
+// courses.map((item,index)=>(
+//     dataY.innerHTML += `
+   
+// <div class="col-12">
+// <div class="row">
+//     <button  id="ht"  onclick="clChange(${index})">
+//         <span></span>
+//         <span
+//         style="color:#0152a8 ;background-color: #e6faff;font-size:x-large;font-weight: 700;padding:0 10px;"
+//         >${item.btns}</span>
+//     </button>
+// </div>
+//     <div id='hg'>
+//     <div class="row">
+
+//     <div class="row text-center justify-content-center mt-3">
+//         <div class="col-3">
+//             <img src=${item.src} alt="Python" class="img-fluid">
+//         </div>
+//     </div>
+
+//     <div class="row text-justify mt-3">
+//         <p>
+// ${item.title}
+//         </p>
+//     </div>
+
+//  </div> 
+//     </div>
+   
+// </div>
+
+//       `
+// ))
+
+// function clChange(get){
+// const got =`${get}`;
+// var eon=document.querySelector('#ht');
+// var eond=document.querySelector('#hg');
+
+
+
+// if(got== 0){
+//     eon.classList.add('test');
+//     eond.classList.add('used');
+
+
+//     eon.classList.remove('test1');
+//     eond.classList.remove('used1');
+  
+//     eon.classList.remove('test2');
+//     eond.classList.remove('used2');
+
+//     eon.classList.remove('test3');
+//     eond.classList.remove('used3');
+
+//     eon.classList.remove('test4');
+//     eond.classList.remove('used4');
+// }
+// else if(got== 1){
+//     eon.classList.add('test1');
+//     eond.classList.add('used1');
+
+//     eon.classList.remove('test');
+//     eond.classList.remove('used');
+  
+//     eon.classList.remove('test2');
+//     eond.classList.remove('used2');
+
+//     eon.classList.remove('test3');
+//     eond.classList.remove('used3');
+
+//     eon.classList.remove('test4');
+//     eond.classList.remove('used4');
+// }
+// else if(got== 2){
+//     eon.classList.add('test2');
+//     eond.classList.add('used2');
+
+//     eon.classList.remove('test1');
+//     eond.classList.remove('used1');
+  
+//     eon.classList.remove('test');
+//     eond.classList.remove('used');
+
+//     eon.classList.remove('test3');
+//     eond.classList.remove('used3');
+
+//     eon.classList.remove('test4');
+//     eond.classList.remove('used4');
+// }
+// else if(got== 3){
+//     eon.classList.add('test3');
+//     eond.classList.add('used3');
+
+//     eon.classList.remove('test1');
+//     eond.classList.remove('used1');
+  
+//     eon.classList.remove('test2');
+//     eond.classList.remove('used2');
+
+//     eon.classList.remove('test');
+//     eond.classList.remove('used');
+
+//     eon.classList.remove('test4');
+//     eond.classList.remove('used4');
+// }
+// else if(got== 4){
+//     eon.classList.add('test4');
+//     eond.classList.add('used4');
+
+//     eon.classList.remove('test1');
+//     eond.classList.remove('used1');
+  
+//     eon.classList.remove('test2');
+//     eond.classList.remove('used2');
+
+//     eon.classList.remove('test3');
+//     eond.classList.remove('used3');
+
+//     eon.classList.remove('test');
+//     eond.classList.remove('used');
+// }
+// }
+
+// console.log(eon);
